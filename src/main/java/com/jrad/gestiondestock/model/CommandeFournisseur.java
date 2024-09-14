067ccb0afc0b3d78158815fdf4f59135b98eb1b1
JradChaima1 <chaimajrad1888@gmail.com>
2024-09-14 15:30:46 +0100

    creation des entités + creation du BD et des tables

---
+++ src/main/java/com/jrad/gestiondestock/model/CommandeFournisseur.java
@@ -0,0 +1,28 @@
+package com.jrad.gestiondestock.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper=true)
+@Entity
+@Table(name = "commandefournisseur")
+public class CommandeFournisseur extends AbstractEntity{
+
+    @Column(name = "code")
+    private String code;
+    @Column(name = "datecommande")
+    private Instant dateCommande;
+    @ManyToOne
+    @JoinColumn(name ="idFournisseur")
+    private Fournisseur fournisseur;
+    @OneToMany(mappedBy = "commandeFournisseur")
+    private List<LigneCommandeFournisseur> ligneCommandeFournisseurs;
+}
---
+++ src/main/java/com/jrad/gestiondestock/model/Ventes.java
@@ -0,0 +1,21 @@
+package com.jrad.gestiondestock.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper=true)
+@Entity
+@Table(name = "ventes")
+public class Ventes  extends AbstractEntity{
+    @ManyToOne
+    @JoinColumn(name = "idarticle")
+    private Article article;
+}
